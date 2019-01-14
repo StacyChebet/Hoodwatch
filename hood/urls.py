a5b2ef5d35f0b2ee5b3d7763898d72e46cfd77aa
StacyChebet <staceychebet23@gmail.com>
2019-01-14 09:02:20 +0300

    Create url for police stations

--- hood/urls.py
+++ hood/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^accounts/profile/$', views.profile, name="profile"),
     url(r'^business/$', views.business, name="business"),
+    url(r'^stations/$,', views.police_stations, name="stations"),
+    
 
 ]
 if settings.DEBUG:
