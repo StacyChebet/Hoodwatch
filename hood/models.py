62c612749f4821230881a933287fa1e931458ec1
StacyChebet <staceychebet23@gmail.com>
2019-01-14 09:28:34 +0300

    Create the business model

--- hood/models.py
+++ hood/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Neighbourhood(models.Model):
-    member = models.ForeignKey(User, on_delete=models.CASCADE)
-    pic = models.ImageField()
-    bio = models.TextField()
+class Business(models.Model):
+    # admin = models.ForeignKey()
+    hood_name = models.CharField(max_length = 40)
+    hood_location = models.CharField(max_length=40)
+    occupants = models.IntegerField(blank=True)
+
+    @classmethod
+    def get_all_businesses(self):
+        businesses = Business.objects.all()
+        return businesses
 
